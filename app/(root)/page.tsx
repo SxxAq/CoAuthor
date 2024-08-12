d9f3e132f0941e2982dc05f432b5869373e20d82
SxxAq <aqueel192@gmail.com>
2024-08-12 14:54:54 +0530

    Room creation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,24 +1,9 @@
-import { Editor } from "@/components/editor/Editor";
-import Header from "@/components/header/Header";
 import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import React from "react";
 
 const page = () => {
   return (
     <div>
-      <Header>
-        <div className="flex w-fit items-center justify-center gap-2">
-          <p className="document-title">Title</p>
-        </div>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </Header>
-      <Editor />
+      <Button>ClickME</Button>
     </div>
   );
 };
